24d450e567833db884a7509a74484bff5ef5c826
Will Li <49330381+williamli9300@users.noreply.github.com>
2025-07-11 03:01:01 -0700

    Update CullRawByJPG.py

--- CullRawByJPG.py
+++ CullRawByJPG.py
@@ -1,4 +1,4 @@
-# CullRawByJPG.py | version 0.0.3 | https://github.com/williamli9300/CullRawByJPG
+# CullRawByJPG.py | version 0.1.0 | https://github.com/williamli9300/CullRawByJPG
 
 import os
 import time
